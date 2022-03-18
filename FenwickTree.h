0726a525edaec67f904a7e1793f65e167248b79f
Anthony Meyer <85413356+ghAnthonyCodes@users.noreply.github.com>
2022-03-18 01:06:39 -0400

    Made generic, added two different operation modes.
    Added an option to perform (range add, point query) vs. (point add, range query). Made class take generic data type, mainly for int vs. long long problems.

--- FenwickTree.h
+++ FenwickTree.h
@@ -2,46 +2,66 @@
 #include <cstdio>
 #include <cmath>
 
+#define RANGE_ADD
+
+template <typename T_t>
 class FenwickTree {
  public:
   int N;
-  std::vector<int> T;
+  std::vector<T_t> T;
 
   // Initiate tree, internal tree is 1-based indexing, but external isn't
   FenwickTree(int N) {
-    this->N = N;
-    T = std::vector<int>(N + 1, 0);
+    this->N = N + 2;
+    this->T = std::vector<T_t>(N + 2, 0);
   }
 
-  void add(int r, int v) {
-    r++;
-    while (r <= N) {
-      this->T[r] += v;
-      r = r + (r & -r);
-    }
+#if defined(RANGE_ADD)
+
+  void add(int x, T_t v) {
+    for (++x; x < this->N; x += x & -x)
+      this->T[x] += v;
+  }
+
+  void rangeAdd(int l, int r, T_t v) {
+    this->add(l, v);
+    this->add(r + 1, -v);
+  }
+
+  T_t pointQuery(int x) {
+    T_t ret = 0;
+    for (++x; x > 0; x -= x & -x)
+      ret += this->T[x];
+    return ret;
+  }
+
+#elif defined(POINT_ADD)
+
+  void add(int x, T_t v) {
+    for (++x; x < this->N; x += x & -x)
+      this->T[x] += v;
   }
 
   // Sums [0 - r] inclusive
-  int sum(int r) {
-    int ret = 0;
-    r++;
-    while (r > 0) {
+  T_t sum(int r) {
+    T_t ret = 0;
+    for (++r; r > 0; r -= r & -r) {
       ret += this->T[r];
-      r = r - (r & -r);
-    }
     return ret;
   }
 
   // Sums [l - r] inclusive
-  int sumRange(int l, int r) {
+  T_t sumRange(int l, int r) {
     return this->sum(r) - this->sum(l - 1);
   }
+
+#endif
 
   // For debugging
   void printTree(void) {
     printf("T = { ");
     for (int i = 1; i <= this->N; i++)
-      printf("%d ", this->T[i]); 
+      printf("%lld ", (ll)this->T[i]); 
     printf("}\n");
   }
 };
