005c8686f74cd87efe098ea6418172f7eed9fc87
ameyer43 <apmeyer@gatech.edu>
2022-03-24 00:44:01 -0400

    Wrote binary exponentiation function

---
+++ BinaryExponentiation.h
@@ -0,0 +1,15 @@
+#define ll long long
+
+/******************************/
+/* returns a^b                */
+/*****************************/
+ll binPow(ll a, ll b) {
+   ll answer = 1;
+   while (b) {
+      if (b & 1)
+         answer *= a;
+      a *= a;
+      b >>= 1;
+   }
+   return answer;
+}
