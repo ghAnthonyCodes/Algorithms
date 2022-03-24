0fe07a84ed359d144583587a4c6b3ebdf9156c05
ameyer43 <apmeyer@gatech.edu>
2022-03-24 00:57:31 -0400

    Added modulo option and prime module with fermats little theorem

--- BinaryExponentiation.h
+++ BinaryExponentiation.h
@@ -1,4 +1,5 @@
 #define ll long long
+#include <cstdio>
 
 // returns a^b
 ll binPow(ll a, ll b) {
@@ -7,6 +8,34 @@ ll binPow(ll a, ll b) {
       if (b & 1)
          answer *= a;
       a *= a;
+      b >>= 1;
+   }
+   return answer;
+}
+
+// returns a^b % M
+ll binPow(ll a, ll b, ll M) {
+
+   ll answer = 1;
+   while (b) {
+      if (b & 1)
+         answer = (answer * a) % M;
+      a = (a * a) % M;
+      b >>= 1;
+   }
+   return answer;
+}
+
+// returns a^b % M but runs faster with fermat's little theorem
+ll binPowPrimeMod(ll a, ll b, ll M) {
+
+   // Fermat's little theorem trick to speed up alg
+   b = b % (M - 1);
+   ll answer = 1;
+   while (b) {
+      if (b & 1)
+         answer = (answer * a) % M;
+      a = (a * a) % M;
       b >>= 1;
    }
    return answer;
